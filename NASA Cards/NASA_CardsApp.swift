e014bfdec3b2590afea8b89c145389bf4082c218
izhuravlev <ivan.zhuravlev@noveogroup.com>
2021-04-22 12:38:51 +0300

    Initial Commit

---
+++ NASA Cards/NASA_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASA_CardsApp.swift
+//  NASA Cards
+//
+//  Created by dtermined on 22.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct NASA_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
